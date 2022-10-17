faed8dc81868cab1b9902d3937d0b6cb55e96fd8
eduardo_alvarez_martinez <eduardo.alvarez.martinez@externos.gloval.es>
2022-10-17 19:41:27 +0200

    first test implemented

--- test/getString.test.js
+++ test/getString.test.js
@@ -1,8 +1,48 @@
-const getString = require("../src/services/getString");
 const knexMock = require("mock-knex");
+const db = require("../src/configs/db/db");
+const tasksService = require("../src/services/tasks");
 
-describe("Test general", () => {
-  test('getString devuelve la cadena de caracteres "test service"', () => {
-    expect(getString()).toBe("test service");
+knexMock.mock(db);
+
+describe("Services tests", () => {
+  beforeEach(() => {
+    tracker = knexMock.getTracker();
+    tracker.install();
+    knexMock.mock(db.connection);
+  });
+
+  afterEach(() => {
+    tracker.uninstall();
+    knexMock.unmock(db.connection);
+  });
+
+  test("Tasks is expecting to return 3 tasks", async () => {
+    tracker.on("query", (query) => {
+      query.response({
+        success: true,
+        info: [
+          {
+            id: 0,
+            title: "Test",
+            content: "Test content changed",
+          },
+          {
+            id: 1,
+            title: "Test 1",
+            content: "This is a test",
+          },
+          {
+            id: 2,
+            title: "Test 2",
+            content: "Another test",
+          },
+        ],
+      });
+    });
+
+    const result = await tasksService();
+
+    expect(result.info.success).toBe(true);
+    expect(result.info.info.length).toBe(3);
   });
 });
